fa390b681fb804233e8a0d240f9c058d2cad7125
Bill Riehl <briehl@gmail.com>
2026-05-16 00:36:21 -0400

    nuked CRA stuff form orbit. It was the only way to be sure.

--- src/routes/Ungar/components/UngarView.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import RaisedButton from 'material-ui/RaisedButton'
-
-export const UngarView = () => (
-    <RaisedButton label='yo mofacka' />
-)
-
-export default UngarView
